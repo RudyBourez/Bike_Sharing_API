923c775d1a4375ae6fcbaa88508e659393ceb562
Rudy <bourez.rudy@gmail.com>
2022-03-30 14:56:13 +0200

    json

--- API/__init__.py
+++ API/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
+import pandas as pd
 from flask_restful import Api
 from API.resources.model import Hello
 
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(Hello, '/')
+api.add_resource(Hello,'/', '/<string:json>')

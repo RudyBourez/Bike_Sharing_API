e4333ab3ceb9849621ed6ae9007846a07a78f9ab
Rudy <bourez.rudy@gmail.com>
2022-03-30 13:45:11 +0200

    procfile crrection api:app

--- API/__init__.py
+++ API/__init__.py
@@ -1,4 +1,3 @@
-from pickle import APPEND
 from flask import Flask
 from flask_restful import Api
 from API.resources.model import Model
@@ -6,4 +5,4 @@ from API.resources.model import Model
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(Model, '/Model' ,'/Model/<string:name>')
+api.add_resource(Model, '/model' ,'/model/<string:name>')

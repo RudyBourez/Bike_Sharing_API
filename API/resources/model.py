94ee47c9c872342e7715e2b4f155e8b7397db586
Rudy <bourez.rudy@gmail.com>
2022-03-30 16:48:37 +0200

    git lfs

--- API/resources/model.py
+++ API/resources/model.py
@@ -4,6 +4,7 @@ import pickle
 
 class Hello(Resource):
     def get(self, json):
+        # Git LFS
         df = pd.DataFrame(eval(json))
         model = pickle.load(open("modele.sav", "rb"))
         y_pred = model.predict(df)

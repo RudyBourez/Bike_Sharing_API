2725e737b4dba1e92ebeea3bb3c68398a474f32f
Rudy <bourez.rudy@gmail.com>
2022-03-30 15:54:30 +0200

    test orient

--- API/resources/model.py
+++ API/resources/model.py
@@ -4,7 +4,7 @@ import pickle
 
 class Hello(Resource):
     def get(self, json):
-        df = pd.read_json(json)
+        df = pd.read_json(json, orient="records")
         model = pickle.load(open("model.sav", "rb"))
         y_pred = model.predict(df)
         df["count"] = y_pred

7aedc8fa6065bff38afbefe1367d0b68ac82bb8c
Rudy <bourez.rudy@gmail.com>
2022-03-30 14:02:22 +0200

    test

--- API/__init__.py
+++ API/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask_restful import Api
-from API.resources.model import Model
+from API.resources.model import Hello
 
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(Model, '/model' ,'/model/<string:name>')
+api.add_resource(Hello, '/')
--- API/resources/model.py
+++ API/resources/model.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource
 
-class Model(Resource):
-    def get(self, name):
-        return {'Task': 'Say,'+ f'"Hello {name}!"'}
+class Hello(Resource):
+    def get(self):
+        return {'Task': 'Say "Hello World!"'}

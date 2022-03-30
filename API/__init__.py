ac3cb9165683b3fb5c1376ddbe1e72b982bab852
Rudy <bourez.rudy@gmail.com>
2022-03-30 13:29:45 +0200

    first commit

---
+++ API/__init__.py
@@ -0,0 +1,11 @@
+from pickle import APPEND
+from flask import Flask
+from flask_restful import Api
+from API.resources.model import Model
+
+app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(Model, '/Model' ,'/Model/<string:name>')
+
+from .resources.routes import *
---
+++ API/resources/routes.py
@@ -0,0 +1,5 @@
+from API import app
+
+@app.route('/')
+def home():
+    return "<h1> Hello World </h1>"

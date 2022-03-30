3750ba54f7fb908994be0b8f7007dac14a0abdd1
Rudy <bourez.rudy@gmail.com>
2022-03-30 16:40:30 +0200

    modele

--- API/resources/model.py
+++ API/resources/model.py
@@ -1,11 +1,11 @@
 from flask_restful import Resource
 import pandas as pd
-import pickle, gzip
+import pickle
 
 class Hello(Resource):
     def get(self, json):
         df = pd.DataFrame(eval(json))
-        model = pickle.load(gzip.open("model.sav", "rb"))
+        model = pickle.load(open("modele.sav", "rb"))
         y_pred = model.predict(df)
         df["count"] = y_pred
         return df.to_json(orient="columns")

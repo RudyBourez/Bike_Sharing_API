b41d42677d5c785685efe7002a3e4fc42f416f59
RudyBourez <bourez.rudy@gmail.com>
2022-03-31 14:06:05 +0200

    fixed api

--- API/resources/model.py
+++ API/resources/model.py
@@ -1,11 +1,13 @@
 from flask_restful import Resource
 import pandas as pd
+import numpy as np
 import pickle
 
 class Hello(Resource):
     def get(self, json):
         df = pd.DataFrame(eval(json))
+        print(df)
         model = pickle.load(open("model.sav", "rb"))
         y_pred = model.predict(df)
-        df["count"] = y_pred
+        df["count"] = np.exp(y_pred)-1
         return df.to_json(orient="columns")
